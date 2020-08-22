8a6dcb9f775a9fc8658b6527a1fa8e0f5ec95990
Ben Mens <ben.mens@gmail.com>
2020-08-22 09:44:59 +0200

    pusehd to github

--- src/main/java/nl/benmens/EvolutionGame.java
+++ src/main/java/nl/benmens/EvolutionGame.java
@@ -2,20 +2,29 @@ package nl.benmens;
 
 import processing.core.PApplet;
 
-
-public class EvolutionGame extends PApplet
-{
+public class EvolutionGame extends PApplet {
+  public static EvolutionGame sharedEvolutionGame;
 
   public void settings() {
+    sharedEvolutionGame = this;
+
     size(800, 600, P2D);
   }
-  
+
+  public void setup() {
+  }
+
+  public void draw() {
+    noStroke();
+    fill(250, 90, 70);
+    rect(0, 0, 100, 100);
+  }
 
   static public void main(String[] passedArgs) {
-        if (passedArgs != null) {
-          PApplet.main(EvolutionGame.class, passedArgs);
-        } else {
-          PApplet.main(EvolutionGame.class);
-        }
-      }
+    if (passedArgs != null) {
+      PApplet.main(EvolutionGame.class, passedArgs);
+    } else {
+      PApplet.main(EvolutionGame.class);
+    }
+  }
 }
