c643ad651dbbc051b88998fdc0a95494fffd7962
Ben Mens <ben.mens@gmail.com>
2020-08-22 09:10:11 +0200

    improved external dependencies

--- src/main/java/nl/benmens/EvolutionGame.java
+++ src/main/java/nl/benmens/EvolutionGame.java
@@ -2,13 +2,16 @@ package nl.benmens;
 
 import processing.core.PApplet;
 
-/**
- * Hello world!
- *
- */
+
 public class EvolutionGame extends PApplet
 {
-    static public void main(String[] passedArgs) {
+
+  public void settings() {
+    size(800, 600, P2D);
+  }
+  
+
+  static public void main(String[] passedArgs) {
         if (passedArgs != null) {
           PApplet.main(EvolutionGame.class, passedArgs);
         } else {
